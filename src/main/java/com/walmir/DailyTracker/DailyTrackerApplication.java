9391a9401f3038bce23ebba1325f2b6dcad08b2d
Walmir Alves <walmirjr915@gmail.com>
2026-02-03 15:38:12 -0300

    Initial commit.

---
+++ src/main/java/com/walmir/DailyTracker/DailyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.walmir.DailyTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walmir/DailyTracker/DailyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmir.DailyTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
